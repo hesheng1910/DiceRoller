024df325f6ebc6d487576e8e544975b0bb6a7c90
hesheng1910 <56876037+hesheng1910@users.noreply.github.com>
2021-04-21 16:57:23 +0700

    Update

--- app/src/androidTest/java/com/example/android/diceroller/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/android/diceroller/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
-package com.example.diceroller
+package com.example.android.diceroller
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,7 +16,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.diceroller", appContext.packageName)
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.example.android.diceroller", appContext.packageName)
     }
 }
